ff6985d299dce0a1a488525883d0f45c29543c21
sithjaisong <sith.jaisong@gmail.com>
2015-02-09 13:51:39 +0800

    polish the script following the style

--- R/3-consistent.R
+++ R/3-consistent.R
@@ -1,30 +1,34 @@
-####################################################
-#'title         : 3-edit
-#'date          : January, 2015
-#'purpose       : Convert ordinal data (to what?)
-#'writed by     : Sith Jaisong (s.jaisong@irri.org)
-#'contact       : International Rice Research Institute
-#'input         : import excel file from the shared files and delete the 
-#'output        : data frame and RData (of what? why?)
+########################Header################################################
+# title         : 3-consistent
+# purpose       : recoding the factors;
+# producer      : prepared by S. Jaisong (s.jaisong@irri.org);
+# last update   : in Los Baños, Laguna, PHL, Jan 2015;
+# inputs        : 2-correct.class.skep1.survey.RData;
+# outputs       : 3-consistant.output.RData;
+# remarks 1     : ;
+# remarks 2     : ;
 #####################################################
-# 
-#---- Set working directory 
+
+#### Set working directory  ####
 # set your working directory
 wd = '~/Documents/R.github/network.analysis.skep1' 
 setwd(wd)
+#### End of set working directory ####
 
-#----Load R.Data file from output folder
-load(file="output/2-correct.class.skep1survey.RData")
+#### Load R.Data file from output folder ####
+load(file = "output/2-correct.class.skep1survey.RData")
+#### End Load E.Data file from output folder ####
 
-# This step is to seelect the numeric data set se
-
+#### Recoding the factor ####
 levels(data$cs)[levels(data$cs) == "very poor"] <- 1
 levels(data$cs)[levels(data$cs) == "poor"] <- 2
 levels(data$cs)[levels(data$cs) == "average"] <- 3
 levels(data$cs)[levels(data$cs) == "good"] <- 4
 levels(data$cs)[levels(data$cs) == "very good"] <- 5
+#### end od recoding the factor ####
 
-save(data, file ="output/3-consistent.output.RData")
 
+#### Save data to R object ####
+save(data, file = "output/3-consistent.output.RData")
+#### end save data ####
 # eos
-
